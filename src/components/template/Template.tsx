12365e1e81b7ac61681317e81270681df90fb8a3
Pranav <pranav.2002tis@gmail.com>
2024-10-17 00:20:12 +0530

    fixed tailwind config

--- src/components/template/Template.tsx
+++ src/components/template/Template.tsx
@@ -10,9 +10,7 @@ const Template = ({
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">{title}</h1>
       </div>
-      <div className="flex flex-1 p-5 rounded-lg border shadow-sm">
-        {children}
-      </div>
+      <div className="flex flex-1 rounded-lg border shadow-sm">{children}</div>
     </main>
   );
 };
