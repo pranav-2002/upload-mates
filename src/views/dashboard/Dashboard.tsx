dc1e8a004aeb210795fa1ef6fef5e5040b66e2e5
Pranav <pranav.2002tis@gmail.com>
2024-10-24 21:10:51 +0530

    Fixed minor build linting issue

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -16,7 +16,7 @@ export default async function Dashboard() {
   return (
     <div className="container mx-auto p-3">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {userChannels.map((channel) => (
+        {userChannels?.map((channel) => (
           <ChannelCard key={channel.id} channel={channel} />
         ))}
       </div>
