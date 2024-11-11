3e01a2e750ab256b635cfd9955a6875f7a42f501
Pranav <pranav.2002tis@gmail.com>
2024-11-11 20:18:51 +0530

    Fixed build emoji error

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,8 +4,7 @@ const Footer = () => {
   return (
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
       <p className="text-xs text-gray-500 dark:text-gray-400">
-        Made with <span className="text-xl text-red-500">&#9829;</span> by
-        Pranav KSK
+        Made by Pranav KSK
       </p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
         <Link
